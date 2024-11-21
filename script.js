1f14c6ac8ae1a48a49f7c748473a54c366a248b0
Sisipho Kanana <sisipho.kanana@capaciti.org.za>
2024-11-21 10:57:08 +0200

    update

--- script.js
+++ script.js
@@ -18,12 +18,12 @@ $(document).ready(function(){
     // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
+        
         $('html').css("scrollBehavior", "auto");
     });
 
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+       
         $('html').css("scrollBehavior", "smooth");
     });
 
@@ -48,7 +48,7 @@ $(document).ready(function(){
         loop: true
     });
 
-    // owl carousel script
+
     $('.carousel').owlCarousel({
         margin: 20,
         loop: true,
